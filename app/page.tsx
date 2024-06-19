d8970b4602448dcf5efff41d539f69ee95011696
NoCode Consulting <connor@nocodeconsulting.com>
2024-06-19 17:37:59 -0500

    added correct playlists

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-"use client"; // Ensure this is the first line in the file
-
+"use client";
 import React, { useEffect, useRef } from 'react';
 
 const Home = () => {
@@ -24,14 +23,14 @@ const Home = () => {
         <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-white px-4 text-center">
           <div className="relative z-10">
             <h1 className="text-5xl md:text-7xl font-bold mb-4 font-sans">NAZARÉ</h1>
-            <p className="text-lg md:text-xl max-w-[800px] mb-8 font-sans">Deep and crispy tropical bass</p>
+            <p className="text-lg md:text-xl max-w-[800px] mb-8 font-sans">Deep and crispy tropical bass 🎧</p>
           </div>
         </div>
       </div>
       <div className="container mx-auto py-12 md:py-20">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex flex-col h-[500px]">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">Essentials Playlist</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">NAZARÉ Essentials</h2>
             <iframe
               allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
               frameBorder="0"
@@ -42,14 +41,14 @@ const Home = () => {
             />
           </div>
           <div className="flex flex-col h-[500px]">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">Artist</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">Recently Added</h2>
             <iframe
               allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
               frameBorder="0"
               height="100%"
               style={{ width: "100%", maxWidth: "660px", overflow: "hidden", borderRadius: "10px", background: "transparent", flex: 1 }}
               sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-              src="https://embed.music.apple.com/us/playlist/nazar%C3%A9-essentials/pl.u-4JomKKNCaAxAqN"
+              src="https://embed.music.apple.com/us/playlist/recently-added/pl.u-06oxvgaCW8R8kA"
             />
           </div>
         </div>

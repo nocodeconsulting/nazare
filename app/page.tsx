bff6d21752d10851b305b259fe374213d8755fe7
NoCode Consulting <connor@nocodeconsulting.com>
2024-06-19 21:19:35 -0500

    added made sans evolve / evo

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,22 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-[100dvh] font-sans">
+    <div className="flex flex-col min-h-[100dvh] font-made-evolve-alt">
       <div className="relative w-full h-[75dvh] overflow-hidden">
         <video ref={videoRef} autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover z-0">
           <source src="https://nocodeconsulting.blob.core.windows.net/nazaremusic/bigwave.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-white px-4 text-center">
           <div className="relative z-10">
-            <h1 className="text-5xl md:text-7xl font-bold mb-4 font-sans">N A Z A R É</h1>
-            <p className="text-lg md:text-xl max-w-[800px] mb-8 font-sans">Deep and crispy tropical bass 🎧</p>
+            <h1 className="text-5xl md:text-7xl font-bold mb-2 font-made-evolve">N A Z A R É</h1>
+            <p className="text-lg md:text-xl max-w-[800px] mb-8 font-normal !font-normal">Deep and crispy tropical bass 🎧</p>
           </div>
         </div>
       </div>
       <div className="container mx-auto py-12 md:py-20">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex flex-col h-[500px]">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">NAZARÉ Essentials</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4">NAZARÉ Essentials</h2>
             <iframe
               allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
               frameBorder="0"
@@ -41,7 +41,7 @@ const Home = () => {
             />
           </div>
           <div className="flex flex-col h-[500px]">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 font-sans">Recently Added</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4">Recently Added</h2>
             <iframe
               allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
               frameBorder="0"
